8c5ca39a9288502e3fe0dd9a2b875f50958ec54b
lukasz-pasieczynski <l.pasieczynski@gmail.com>
2020-06-21 16:59:40 +0200

    BookApp

---
+++ src/main/java/pl/coderslab/warsztat5krkw04/controller/BookController.java
@@ -0,0 +1,55 @@
+package pl.coderslab.warsztat5krkw04.controller;
+
+import org.springframework.web.bind.annotation.*;
+import pl.coderslab.warsztat5krkw04.model.Book;
+import pl.coderslab.warsztat5krkw04.service.MemoryBookService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    private MemoryBookService mbs;
+
+    public BookController(MemoryBookService mbs) {
+        this.mbs = mbs;
+    }
+
+    @GetMapping("/helloBook")
+    public Book helloBook() {
+        return new Book(1L, "9788324631766", "Thinking in Java",
+                "Bruce Eckel", "Helion", "programming");
+    }
+
+    @GetMapping("")
+    public List<Book> getAll() {
+        return this.mbs.getList();
+    }
+
+    @GetMapping("/{id}")
+    public Book getById(@PathVariable long id) {
+        return this.mbs.getById(id);
+    }
+
+    @PostMapping("")
+    public String addBook(@RequestBody Book book) {
+        this.mbs.addBook(book);
+        return "{\"status\": \"ok\"}";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteBook(@PathVariable long id) {
+        this.mbs.deleteById(id);
+        return "{}";
+    }
+
+    @PutMapping("/{id}")
+    public String editBook(@RequestBody Book book) {
+        this.mbs.editBook(book);
+        return "{}";
+    }
+
+
+}
+
+
---
+++ src/main/java/pl/coderslab/warsztat5krkw04/service/MemoryBookService.java
@@ -0,0 +1,67 @@
+package pl.coderslab.warsztat5krkw04.service;
+
+import org.springframework.stereotype.Service;
+import pl.coderslab.warsztat5krkw04.model.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MemoryBookService {
+    private long nextId;
+    private List<Book> list;
+
+    public MemoryBookService() {
+        list = new ArrayList<>();
+        list.add(new Book(1L, "9788324631766", "Thinking in Java", "Bruce Eckel",
+                "Helion", "programming"));
+        list.add(new Book(2L, "9788324627738", "Rusz glowa, Java.",
+                "Sierra Kathy, Bates Bert", "Helion", "programming"));
+        list.add(new Book(3L, "9780130819338", "Java 2. Podstawy",
+                "Cay Horstmann, Gary Cornell", "Helion", "programming"));
+        // W KONSTRUKTORZE
+        setNextId();
+    }
+
+    private void setNextId() {
+        this.nextId = this.list.stream()
+                .mapToLong(Book::getId)
+                .max()
+                .orElse(0L) + 1;
+    }
+
+    public List<Book> getList() {
+        return list;
+    }
+
+    public void setList(List<Book> list) {
+        this.list = list;
+    }
+
+    public Book getById(long id) {
+        return this.list.stream()
+                .filter(book -> book.getId() == id)
+                .findAny()
+                .orElse(null);
+    }
+
+    public void addBook(Book toAdd) {
+        if (this.list.stream().anyMatch(book -> book.getId() == toAdd.getId())) {
+            return;
+        }
+        toAdd.setId(this.nextId++);
+        this.list.add(toAdd);
+    }
+
+    public void deleteById(long id) {
+        final Book book = getById(id);
+        this.list.remove(book);
+    }
+
+    public void editBook(Book toEdit) {
+        Book bookToEdit = getById(toEdit.getId());
+        bookToEdit.merge(toEdit);
+
+
+    }
+}
